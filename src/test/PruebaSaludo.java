1d90e2678ecf51748e091770cbdc18d0025cc069
conce <conce@172.20.167.192>
2022-04-13 09:56:21 -0300

    Updated

---
+++ src/test/PruebaSaludo.java
@@ -0,0 +1,16 @@
+
+package test;
+
+import saludo.HolaMundo;
+
+public class PruebaSaludo {
+    
+    public static void main(String[] args) {
+        
+        HolaMundo saludo1=new HolaMundo();
+       
+        saludo1.cargar();
+        saludo1.imprimir();
+        
+    }
+}
